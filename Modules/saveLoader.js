c1e0a0647732342e01d0cd683dfdab66795918b5
MolSnoo <anderson.jared.16@gmail.com>
2020-01-18 18:04:52 -0500

    Fixed saveLoader module to avoid writing Circular references.

--- Modules/saveLoader.js
+++ Modules/saveLoader.js
@@ -1,9 +1,20 @@
 ﻿const settings = include('settings.json');
-
 const fs = require('fs');
 
+const Player = include(`${settings.dataDir}/Player.js`);
+
 module.exports.save = function (game) {
-    const gameJson = JSON.stringify(game);
+    var data = {
+        inProgress: game.inProgress,
+        canJoin: game.canJoin,
+        halfTimer: game.halfTimer,
+        endTimer: game.endTimer,
+        players: []
+    };
+    for (let i = 0; i < game.players.length; i++)
+        data.players.push(new Player(game.players[i].id, null, game.players[i].name, game.players[i].alive));
+
+    const gameJson = JSON.stringify(data);
     return new Promise((resolve) => {
         fs.writeFile(settings.savedDataFileName, gameJson, 'utf8', function (err) {
             if (err) return console.log(err);
