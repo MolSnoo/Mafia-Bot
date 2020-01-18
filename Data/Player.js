0b6bf3f209253de06b77c9a29a221f98464923a2
MolSnoo <anderson.jared.16@gmail.com>
2020-01-18 17:45:17 -0500

    Added play_eligible command.

---
+++ Data/Player.js
@@ -0,0 +1,12 @@
+﻿const settings = include('settings.json');
+
+class Player {
+    constructor(id, member, name, alive) {
+        this.id = id;
+        this.member = member;
+        this.name = name;
+        this.alive = alive;
+    }
+}
+
+module.exports = Player;
