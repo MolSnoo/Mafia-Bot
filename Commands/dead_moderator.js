04aa15443e50207e922c32b1f62f28e9bf8d56f0
MolSnoo <anderson.jared.16@gmail.com>
2020-01-19 17:41:14 -0500

    Added dead_moderator command.

---
+++ Commands/dead_moderator.js
@@ -0,0 +1,24 @@
+﻿const settings = include('settings.json');
+
+module.exports.config = {
+    name: "dead_moderator",
+    description: "Lists all dead players.",
+    details: "Lists all dead players.",
+    usage: `${settings.commandPrefix}dead\n`
+        + `${settings.commandPrefix}died`,
+    usableBy: "Moderator",
+    aliases: ["dead", "died"],
+    requiresGame: true
+};
+
+module.exports.run = async (bot, game, message, command, args) => {
+    var playerList = "Dead players:\n";
+    const deadPlayers = game.players.filter(player => player.alive === false);
+    if (deadPlayers.length > 0)
+        playerList += deadPlayers[0].name;
+    for (let i = 1; i < deadPlayers.length; i++)
+        playerList += `, ${deadPlayers[i].name}`;
+    message.channel.send(playerList);
+
+    return;
+};
